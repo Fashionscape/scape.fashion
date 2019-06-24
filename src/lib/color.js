14d3adc86cc437e0d0d0b72f3bb39e5efc619159
Nick Pierson <me@nick.exposed>
2019-06-24 21:38:20 +0000

    fix item sorting bug

--- src/lib/color.js
+++ src/lib/color.js
@@ -1,13 +1,14 @@
 const colordiff = require('color-difference');
 
 const compare = ([a, b]) => colordiff.compare(a, b);
+const byValue = (a, b) => a - b;
 
 const withMatch = colors => item => {
   if (!item.colors || !item.colors.length) return item;
 
   const combinations = colors.map(c => item.colors.map(cc => [c, cc]));
   const matches = [].concat(...combinations).map(compare);
-  const match = matches.slice().sort()[0];
+  const match = matches.slice().sort(byValue)[0];
 
   return {...item, match};
 };
