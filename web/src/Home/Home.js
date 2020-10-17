0c11b654445fe4fffcbb1fc2937bb2625e215404
Nick Pierson <me@nick.exposed>
2020-10-17 19:54:09 +0000

    add footer to home page

--- web/src/Home/Home.js
+++ web/src/Home/Home.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import {Typography} from '@material-ui/core';
+import React from "react";
+import { Container, Typography, Link, Box } from "@material-ui/core";
 
-import Header from './Header';
-import SearchForm from './SearchForm';
-import Section from 'components/Section';
-import Page from 'components/Page';
+import Header from "./Header";
+import SearchForm from "./SearchForm";
+import Section from "components/Section";
+import Page from "components/Page";
 
 const Home = () => {
   return (
@@ -21,7 +21,22 @@ const Home = () => {
           <SearchForm />
         </Section>
       </Page>
+      <Footer />
     </>
+  );
+};
+
+const Footer = () => {
+  return (
+    <Box padding={2} textAlign="center">
+      <Link href="https://discord.gg/uFv57D5" variant="body2">
+        Discord
+      </Link>
+      <Box display="inline" m={1} />
+      <Link href="https://www.patreon.com/nickontheweb" variant="body2">
+        Patreon
+      </Link>
+    </Box>
   );
 };
 
